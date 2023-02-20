a4658163a70c75971edcc288411469a927e6c568
Ruchira <sewminiranasinghe111@gmail.com>
2023-02-20 19:57:57 +0530

    create newprofile.dart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,10 @@
+import 'package:desktopcatchmehigher/newprofile.dart';
 import 'package:flutter/material.dart';
-import 'dart:ui';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Catch-Me-Higher Desktop Application',
-    home: WelcomeForm(),
+    routes: {
+      'newprofile':(context) => NewProfile()
+    },
   ));
-}
-
-class WelcomeForm extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _WelcomeFormState();
-  }
-}
-
-class _WelcomeFormState extends State<WelcomeForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome to CatchMeHigher'),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            getImageAsset(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget getImageAsset() {
-    AssetImage assetImage = AssetImage('images/welcomeImg.jpg');
-    Image image = Image(image: assetImage, width: 800.0, height: 800.0);
-
-    return Container(
-      child: image,
-    );
-  }
 }
---
+++ lib/newprofile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewProfile extends StatefulWidget {
+  const NewProfile({Key? key}) : super(key: key);
+
+  @override
+  _NewProfileState createState() => _NewProfileState();
+}
+
+class _NewProfileState extends State<NewProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
