bf2de4d2bbf1ebe1fb110f90e0cae341cd8204fc
Ruchira <sewminiranasinghe111@gmail.com>
2023-03-23 18:56:45 +0530

    added buttons on activity three page on color selection main page

--- lib/selectionThree.dart
+++ lib/selectionThree.dart
@@ -1,3 +1,4 @@
+import 'package:desktopcatchmehigher/selectionTwo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -39,9 +40,114 @@ class _SelectionActivityThreePageState
               top: 100.0,
               left: 200.0,
               child: Image.asset(
-                'images/seaLion.jpg',
+                'images/sea.jpeg',
                 height: 400.0,
                 width: 400.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 530.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Green",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Blue",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Orange",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text(
+                        "Grey",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 300.0, left: 80.0),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SelectionActivityTwoPage()),
+                  );
+                },
+                child: Image.asset(
+                  'images/left.jpg',
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 300.0, left: 1350.0, right: 30.0),
+              child: InkWell(
+                onTap: () {},
+                child: Image.asset(
+                  'images/right.jpg',
+                  width: 100.0,
+                  height: 100.0,
+                ),
               ),
             ),
           ],
