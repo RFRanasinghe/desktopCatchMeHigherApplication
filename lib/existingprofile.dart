f6fa9bf1514c3ca09accbaa7832e0924ee40c86c
Ruchira <sewminiranasinghe111@gmail.com>
2023-05-06 16:10:33 +0530

    added icons on existing profile and retrieved data

--- lib/existingprofile.dart
+++ lib/existingprofile.dart
@@ -14,11 +14,11 @@ class ExistingProfile extends StatefulWidget {
 class _ExistingProfileState extends State<ExistingProfile> {
   final _formKey = GlobalKey<FormState>();
 
-  // Future<QuerySnapshot> getData() async {
-  //   final userCollection = FirebaseFirestore.instance.collection('students');
-  //   final querySnapshot = await userCollection.get();
-  //   return querySnapshot;
-  // }
+  Future<QuerySnapshot> getData() async {
+    final userCollection = FirebaseFirestore.instance.collection("students");
+    final querySnapshot = await userCollection.get();
+    return querySnapshot;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,29 +28,34 @@ class _ExistingProfileState extends State<ExistingProfile> {
           child: Text("Existing Profile"),
         ),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('students').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+      body: FutureBuilder<QuerySnapshot>(
+        future: getData(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
           }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          }
+
+          final data = snapshot.data!.docs;
+
           return ListView.builder(
-            itemCount: snapshot.data?.docs.length,
-            itemBuilder: (context, index) {
-              var item = snapshot.data?.docs[index].data();
-              return Row(
-                children: [
-                  ListTile(
-                    title: Text(item!['name'] ?? ''),
-                    subtitle: Text(item['condition'] ?? ''),
-                    // trailing: ElevatedButton(
-                    //   onPressed: () {},
-                    //   child: Text("Activity Home"),
-                    // ),
-                  ),
-                ],
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              final dynamic item = data[index].data();
+              return ListTile(
+                title: Text(item?['name']),
+                subtitle: Text(item?['condition']),
+                leading: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.local_activity_rounded),
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.fact_check_sharp),
+                  onPressed: () {},
+                ),
               );
             },
           );
